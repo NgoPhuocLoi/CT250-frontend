f62d8139355d35084997f96e32382318f9d37172
NgoPhuocLoi <phuocloi11223@gmail.com>
2024-03-14 14:24:45 +0700

    Handle filtering coupons

--- src/components/profile/VoucherList.vue
+++ src/components/profile/VoucherList.vue
@@ -5,13 +5,19 @@
     >
       <div class="text-3xl text-center mb-3 mt-3">Kho voucher</div>
       <div>
-        <VoucherNav />
-        <div class="w-full grid grid-cols-2 gap-5 mt-8 px-4">
-          <VoucherCard
-            v-for="collectedCoupon in collectedCoupons"
-            :key="collectedCoupon.id"
-            :voucher="collectedCoupon.coupon"
-          />
+        <VoucherNav v-model="chosenCouponStatus" />
+        <div v-if="filteredCoupons.length > 0">
+          <div class="w-full grid grid-cols-2 gap-5 mt-8 px-4">
+            <VoucherCard
+              v-for="collectedCoupon in filteredCoupons"
+              :key="collectedCoupon.id"
+              :voucher="collectedCoupon.coupon"
+            />
+          </div>
+        </div>
+
+        <div v-else class="text-center text-xl mt-8">
+          Không có mã giảm giá nào
         </div>
       </div>
     </div>
@@ -22,18 +28,40 @@
 import VoucherNav from "./VoucherNav.vue";
 import VoucherCard from "./VoucherCard.vue";
 import couponService from "@/services/coupon";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
+import {
+  COUPON_ALL,
+  COUPON_EXPIRED,
+  COUPON_NEW,
+  COUPON_USED,
+} from "@/constants/couponStatus";
 
 const collectedCoupons = ref([]);
+const filteredCoupons = ref([]);
+const chosenCouponStatus = ref(COUPON_ALL);
 
 onMounted(async () => {
   await fetchCollectedCoupon();
+});
+
+watch(chosenCouponStatus, () => {
+  filteredCoupons.value = collectedCoupons.value.filter((collectedCoupon) => {
+    const predicateMapping = {
+      [COUPON_ALL]: true,
+      [COUPON_NEW]: collectedCoupon.used === false,
+      [COUPON_USED]: collectedCoupon.used === true,
+      [COUPON_EXPIRED]: new Date(collectedCoupon.coupon.endDate) < new Date(),
+    };
+
+    return predicateMapping[chosenCouponStatus.value];
+  });
 });
 
 async function fetchCollectedCoupon() {
   try {
     const res = await couponService.getCollectedCoupons();
     collectedCoupons.value = res.metadata;
+    filteredCoupons.value = res.metadata;
   } catch (error) {
     console.log(error);
   }
---
+++ src/constants/couponStatus.js
@@ -0,0 +1,11 @@
+export const COUPON_ALL = "Tất cả";
+export const COUPON_NEW = "Mới";
+export const COUPON_USED = "Đã dùng";
+export const COUPON_EXPIRED = "Hết hạn";
+
+export const COUPON_STATUSES = [
+  COUPON_ALL,
+  COUPON_NEW,
+  COUPON_USED,
+  COUPON_EXPIRED,
+];
